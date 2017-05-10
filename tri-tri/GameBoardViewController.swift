4c0efedb8efa4943f6c653463fbbb49260e23a6f
feirantonyfigo <feirantonyfigo.hu@gmail.com>
2017-05-10 18:42:09 +0800

    Recreate outlet connection to GameBoardController (deleted codes in ViewController)

---
+++ tri-tri/GameBoardViewController.swift
@@ -0,0 +1,46 @@
+//
+//  GameBoardViewController.swift
+//  tri-tri
+//
+//  Created by Feiran Hu on 2017-05-10.
+//  Copyright © 2017 mac. All rights reserved.
+//
+
+import UIKit
+
+class GameBoardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBOutlet weak var MarkBoard: UILabel!
+    
+    var temp_mark_str = String()
+    var temp_mark_int = 0
+    @IBAction func Mark_Mannual_Increment_Test(_ sender: Any) {
+        temp_mark_str = MarkBoard.text!
+        temp_mark_int = Int(temp_mark_str)!
+        temp_mark_int += 1
+        temp_mark_str = String(temp_mark_int)
+        MarkBoard.text = temp_mark_str
+
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
