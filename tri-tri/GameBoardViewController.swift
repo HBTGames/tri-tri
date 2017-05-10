b70f4cd08ad8222f64f5729cc2102551c2384c48
feirantonyfigo <feirantonyfigo.hu@gmail.com>
2017-05-10 19:25:42 +0800

    Create outlet connection variable for every triangle within game board and create color change function with UIimage extension tint function

--- tri-tri/GameBoardViewController.swift
+++ tri-tri/GameBoardViewController.swift
@@ -20,33 +20,95 @@ class GameBoardViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
     
+    
+    //----------------------------------------------------
+    //outlet connection variable for each triangle in gameboard
+    //name follows protocol:
+    //  "tri_(row)_(column)"
+    @IBOutlet weak var tri_0_0: UIImageView!
+    @IBOutlet weak var tri_0_1: UIImageView!
+    @IBOutlet weak var tri_0_2: UIImageView!
+    @IBOutlet weak var tri_0_3: UIImageView!
+    @IBOutlet weak var tri_0_4: UIImageView!
+    @IBOutlet weak var tri_0_5: UIImageView!
+    @IBOutlet weak var tri_0_6: UIImageView!
+    @IBOutlet weak var tri_1_0: UIImageView!
+    @IBOutlet weak var tri_1_1: UIImageView!
+    @IBOutlet weak var tri_1_2: UIImageView!
+    @IBOutlet weak var tri_1_3: UIImageView!
+    @IBOutlet weak var tri_1_4: UIImageView!
+    @IBOutlet weak var tri_1_5: UIImageView!
+    @IBOutlet weak var tri_1_6: UIImageView!
+    @IBOutlet weak var tri_1_7: UIImageView!
+    @IBOutlet weak var tri_1_8: UIImageView!
+    @IBOutlet weak var tri_2_0: UIImageView!
+    @IBOutlet weak var tri_2_1: UIImageView!
+    @IBOutlet weak var tri_2_2: UIImageView!
+    @IBOutlet weak var tri_2_3: UIImageView!
+    @IBOutlet weak var tri_2_4: UIImageView!
+    @IBOutlet weak var tri_2_5: UIImageView!
+    @IBOutlet weak var tri_2_6: UIImageView!
+    @IBOutlet weak var tri_2_7: UIImageView!
+    @IBOutlet weak var tri_2_8: UIImageView!
+    @IBOutlet weak var tri_2_9: UIImageView!
+    @IBOutlet weak var tri_2_10: UIImageView!
+    @IBOutlet weak var tri_3_0: UIImageView!
+    @IBOutlet weak var tri_3_1: UIImageView!
+    @IBOutlet weak var tri_3_2: UIImageView!
+    @IBOutlet weak var tri_3_3: UIImageView!
+    @IBOutlet weak var tri_3_4: UIImageView!
+    @IBOutlet weak var tri_3_5: UIImageView!
+    @IBOutlet weak var tri_3_6: UIImageView!
+    @IBOutlet weak var tri_3_7: UIImageView!
+    @IBOutlet weak var tri_3_8: UIImageView!
+    @IBOutlet weak var tri_3_9: UIImageView!
+    @IBOutlet weak var tri_3_10: UIImageView!
+    @IBOutlet weak var tri_4_0: UIImageView!
+    @IBOutlet weak var tri_4_1: UIImageView!
+    @IBOutlet weak var tri_4_2: UIImageView!
+    @IBOutlet weak var tri_4_3: UIImageView!
+    @IBOutlet weak var tri_4_4: UIImageView!
+    @IBOutlet weak var tri_4_5: UIImageView!
+    @IBOutlet weak var tri_4_6: UIImageView!
+    @IBOutlet weak var tri_4_7: UIImageView!
+    @IBOutlet weak var tri_4_8: UIImageView!
+    @IBOutlet weak var tri_5_0: UIImageView!
+    @IBOutlet weak var tri_5_1: UIImageView!
+    @IBOutlet weak var tri_5_2: UIImageView!
+    @IBOutlet weak var tri_5_3: UIImageView!
+    @IBOutlet weak var tri_5_4: UIImageView!
+    @IBOutlet weak var tri_5_5: UIImageView!
+    @IBOutlet weak var tri_5_6: UIImageView!
+  //----------------------------------------------------
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //outlet connection variable for MarkBoard (top left)
     @IBOutlet weak var MarkBoard: UILabel!
     
+    //initialize two temporary mark value
     var temp_mark_str = String()
     var temp_mark_int = 0
+    //implement the function to increment 1 whenever press "test" button
     @IBAction func Mark_Mannual_Increment_Test(_ sender: UIButton) {
         temp_mark_str = MarkBoard.text!
         temp_mark_int = Int(temp_mark_str)!
         temp_mark_int += 1
         temp_mark_str = String(temp_mark_int)
         MarkBoard.text = temp_mark_str
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+        //change color of tri_4_6 
+        tri_4_6.image = UIImage(named:"grey_tir_downwards")?.tint(color: UIColor.cyan,blendMode: .destinationIn)   }
     
     
     
